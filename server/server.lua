844e78be98befc58409ee3619ed4721ffa907861
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2023-01-25 05:14:50 -0500

    add translate to spanish and english

--- server/server.lua
+++ server/server.lua
@@ -19,7 +19,7 @@ AddEventHandler('rsg-goldpanning:server:reward', function()
 			-- add items
 			Player.Functions.AddItem(item1, Config.SmallRewardAmount)
 			TriggerClientEvent("inventory:client:ItemBox", src, RSGCore.Shared.Items[item1], "add")
-			TriggerClientEvent('RSGCore:Notify', src, 'not much in this pan', 'primary')
+			TriggerClientEvent('RSGCore:Notify', src, Lang:t('primary.not_much_this_pan'), 'primary')
 		elseif chance >= 50 and chance <= 80 then -- medium reward
 			local item1 = Config.RewardItems[math.random(1, #Config.RewardItems)]
 			local item2 = Config.RewardItems[math.random(1, #Config.RewardItems)]
@@ -28,7 +28,7 @@ AddEventHandler('rsg-goldpanning:server:reward', function()
 			TriggerClientEvent("inventory:client:ItemBox", src, RSGCore.Shared.Items[item1], "add")
 			Player.Functions.AddItem(item2, Config.MediumRewardAmount)
 			TriggerClientEvent("inventory:client:ItemBox", src, RSGCore.Shared.Items[item2], "add")
-			TriggerClientEvent('RSGCore:Notify', src, 'looks like good gold', 'primary')
+			TriggerClientEvent('RSGCore:Notify', src, Lang:t('primary.looks_like_good_gold'), 'primary')
 		elseif chance > 80 then -- large reward
 			local item1 = Config.RewardItems[math.random(1, #Config.RewardItems)]
 			local item2 = Config.RewardItems[math.random(1, #Config.RewardItems)]
@@ -40,9 +40,9 @@ AddEventHandler('rsg-goldpanning:server:reward', function()
 			TriggerClientEvent("inventory:client:ItemBox", src, RSGCore.Shared.Items[item2], "add")
 			Player.Functions.AddItem(item3, Config.LargeRewardAmount)
 			TriggerClientEvent("inventory:client:ItemBox", src, RSGCore.Shared.Items[item3], "add")
-			TriggerClientEvent('RSGCore:Notify', src, 'gold fever jackpot..', 'primary')
+			TriggerClientEvent('RSGCore:Notify', src, Lang:t('primary.gold_fever_jackpot'), 'primary')
 		end
 	else
-		TriggerClientEvent('RSGCore:Notify', src, 'no gold this time..', 'primary')
+		TriggerClientEvent('RSGCore:Notify', src, Lang:t('primary.no_gold_this_time'), 'primary')
 	end
 end)
